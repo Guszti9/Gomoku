7faf1f3e0eddfe0eff519f098402c7e32b05619c
Borbély Tamás <borblytams@gmail.com>
2021-08-17 11:22:17 +0200

    adding todo.js file to store todos in scrum methodology

---
+++ todo.js
@@ -0,0 +1,71 @@
+export const todo = [
+    {
+        BACKLOG: [],
+        TODO: [
+            {
+                id: 1,
+                task: 'implement functions',
+                subtasks: [
+                    {
+                        id: 1,
+                        subtask_1: 'has_won()',
+                        done: undefined,
+                        question: [],
+                        problems: [],
+                        ideas: [],
+                        tasks: [],
+                        time_spent: "",
+                    },
+                    {
+                        id: 2,
+                        subtask_1: 'print_result()',
+                        done: undefined,
+                        question: [],
+                        problems: [],
+                        ideas: [],
+                        tasks: [],
+                        time_spent: undefined,
+                    },
+                    {
+                        id: 3,
+                        subtask_1: 'print_board()',
+                        done: undefined,
+                        question: [],
+                        problems: [],
+                        ideas: [],
+                        tasks: [],
+                        time_spent: undefined,
+                    },
+                    {
+                        id: 4,
+                        subtask_1: 'tictactoe_game()',
+                        done: undefined,
+                        question: [],
+                        problems: [],
+                        ideas: [],
+                        tasks: [],
+                        time_spent: undefined,
+                    },
+                    {
+                        id: 5,
+                        subtask_1: 'main_menu()',
+                        done: undefined,
+                        question: [],
+                        problems: [],
+                        ideas: [],
+                        tasks: [],
+                        time_spent: undefined,
+                    }
+                ]
+            }
+        ],
+        IN_PROGRESS: [],
+        DONE: []
+    }
+]
+
+
+
+todo = {
+    "todo1": ""
+}
